ec5e384db08f23e3fa4a48b9499894a07c353e6a
Carthiana Joseph <jcathiana2000@gmail.com>
2025-06-16 23:15:55 -0400

    Data.js

---
+++ Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "mkkhhg",
+    name: "Bracelet",
+    price: 50 ,
+    desc: "A timeless piece that adds sophistication to every gesture.",
+    img: "image/bracelet2.png",
+  },
+  {
+    id: "nnnjhv",
+    name: "Elegant Ring",
+    price: 100,
+    desc: "Delicate and modern, perfect for everyday elegance.",
+    img: "image/Ring3.png",
+  },
+  {
+    id: "hjgff",
+    name: "Ring",
+    price: 45,
+    desc: "Shine with simplicity.",
+    img: "image/ring-elegente.png",
+  },
+  {
+    id: "erfvbb",
+    name: "necklace",
+    price: 325,
+    desc: "Small detail, big impact.",
+    img: "image/chain2.png",
+  },
+];
